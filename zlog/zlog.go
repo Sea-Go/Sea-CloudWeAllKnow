19209e11e02accdf7f14e5685509c9f4eb9fa7c1
夏尾草 <1910541664@qq.com>
2026-01-17 19:33:48 +0800

    feat: add zlog logger

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -1,1 +1,94 @@
 package zlog
+
+import (
+	"os"
+
+	"github.com/toolkits/pkg/logger"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var zlog *zap.Logger
+
+// logpath 日志文件路径
+// loglevel 日志级别
+func InitLogger(logpath string, loglevel string) {
+	// 日志分割
+	hook := lumberjack.Logger{
+		Filename:   logpath, // 日志文件路径，默认 os.TempDir()
+		MaxSize:    10,      // 每个日志文件保存10M，默认 100M
+		MaxBackups: 30,      // 保留30个备份，默认不限
+		MaxAge:     7,       // 保留7天，默认不限
+		Compress:   true,    // 是否压缩，默认不压缩
+	}
+	write := zapcore.AddSync(&hook)
+	// 设置日志级别
+	// debug 可以打印出 info debug warn
+	// info  级别可以打印 warn info
+	// warn  只能打印 warn
+	// debug->info->warn->error
+	var level zapcore.Level
+	switch loglevel {
+	case "debug":
+		level = zap.DebugLevel
+	case "info":
+		level = zap.InfoLevel
+	case "error":
+		level = zap.ErrorLevel
+	default:
+		level = zap.InfoLevel
+	}
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+	// 设置日志级别
+	atomicLevel := zap.NewAtomicLevel()
+	atomicLevel.SetLevel(level)
+	core := zapcore.NewCore(
+		// zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.NewJSONEncoder(encoderConfig),
+		// zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&write)), // 打印到控制台和文件
+		write,
+		level,
+	)
+	// 开启开发模式，堆栈跟踪
+	caller := zap.AddCaller()
+	// 开启文件及行号
+	development := zap.Development()
+	// 设置初始化字段,如：添加一个服务器名称
+	filed := zap.Fields(zap.String("serviceName", "serviceName"))
+	// 构造日志
+	zlog = zap.New(core, caller, development, filed)
+	logger.Info("DefaultLogger init success")
+}
+func L() *zap.Logger {
+	if zlog == nil {
+		// 防止忘记 Init 导致 panic：给一个 fallback
+		l, _ := zap.NewProduction()
+		return l
+	}
+	return zlog
+}
+
+// S 返回 sugared（printf 风格）
+func S() *zap.SugaredLogger {
+	return L().Sugar()
+}
+
+// Sync 程序退出前调用，确保日志刷盘
+func Sync() {
+	_ = L().Sync()
+	_ = os.Stdout.Sync()
+}
