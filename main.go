c654466108304c23c364c2f5c7443161dba4f355
夏尾草 <1910541664@qq.com>
2026-01-18 21:29:02 +0800

    chore(main): update app bootstrap

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"net/http"
+	"sea/config"
 	"sea/infra"
 	"sea/zlog"
 
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 func main() {
 	zlog.InitLogger("./log/Recommand.log", "debug")
 	zlog.L().Info("service started")
-
-	err := infra.Milvus_Init()
+	defer zlog.Sync()
+	err := config.Load("./config.yaml")
 	if err != nil {
-		zlog.L().Error("infra init failed",
+		zlog.L().Error("config load failed",
 			zap.Error(err))
 		panic(err)
 	}
-
+	err = infra.MilvusInit()
+	if err != nil {
+		zlog.L().Error("milvus init failed",
+			zap.Error(err))
+		panic(err)
+	}
+	err = infra.Neo4jInit()
+	if err != nil {
+		zlog.L().Error("neo4j init failed",
+			zap.Error(err))
+		panic(err)
+	}
+	// 临时这么写，之后改
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
